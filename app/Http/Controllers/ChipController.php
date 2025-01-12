0be4cb5c930ffbe1d6d957c08d34a4d6aaaaa03e
cosmah <cosmahke4@gmail.com>
2025-01-12 15:11:06 +0300

    first commit

---
+++ app/Http/Controllers/ChipController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chip;
+use Illuminate\Http\Request;
+// mine
+use Illuminate\Http\Response;
+use Illuminate\View\View;
+
+class ChipController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        //render blade view
+        return view('chips.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        // 
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chip $chip)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chip $chip)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Chip $chip)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chip $chip)
+    {
+        //
+    }
+}
